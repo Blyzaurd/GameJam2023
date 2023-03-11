cb43bcd4c65063185a05813117b3138a882206d5
P4tofe <siletourneau@cablevision.qc.ca>
2023-03-10 22:05:03 -0500

    début ngIf

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobalService } from './global.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  constructor(public globalService: GlobalService) {}
 }
