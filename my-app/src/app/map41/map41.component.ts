c51cc48b93c9d0e39cdcfe5e76735a77a9ed138e
P4tofe <siletourneau@cablevision.qc.ca>
2023-03-11 18:12:18 -0500

    fin sound exploration1

--- my-app/src/app/map41/map41.component.ts
+++ my-app/src/app/map41/map41.component.ts
@@ -12,19 +12,21 @@ export class Map41Component implements OnInit {
   audio41 = new Audio();
 
   ngOnInit(): void {
-    this.audio00.src = "../assets/Music-Exploration1.wav";
-    this.audio00.load();
-    this.audio00.play();
+    this.audio41.src = "../assets/Music-Exploration1.wav";
+    this.audio41.load();
+    this.audio41.play();
   }
 
   goToMap42() {
     this.globalService.player.positionX = 4;
     this.globalService.player.positionY = 2;
+    this.audio41.pause();
   }
 
   goToMap40() {
     this.globalService.player.positionX = 4;
     this.globalService.player.positionY = 0;
+    this.audio41.pause();
   }
 
 
