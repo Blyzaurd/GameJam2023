ca684ecb3d920cf2aa86881f06f7747f1e486d4f
P4tofe <siletourneau@cablevision.qc.ca>
2023-03-12 10:20:10 -0400

    musique accueil

--- my-app/src/app/accueil/accueil.component.ts
+++ my-app/src/app/accueil/accueil.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GlobalService } from '../global.service';
 
 @Component({
@@ -6,14 +6,23 @@ import { GlobalService } from '../global.service';
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit {
 
   constructor(private globalService: GlobalService){}
+
+  audioOuverture = new Audio();
+
+  ngOnInit(): void {
+    this.audioOuverture.src = "/assets/Music_OuvertureMenu.wav";
+    this.audioOuverture.load();
+    this.audioOuverture.play();
+  }
 
   goToMenu(){
     this.globalService.player.positionX = 100;
     this.globalService.player.positionY = 100;
-    
+    this.audioOuverture.pause();
+
   }
 
 }
