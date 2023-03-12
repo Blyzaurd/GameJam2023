616469df1f2cd4c9692939ad872321ea457ce7d1
P4tofe <siletourneau@cablevision.qc.ca>
2023-03-11 21:01:39 -0500

    reset lors mort

--- my-app/src/app/gameover/gameover.component.ts
+++ my-app/src/app/gameover/gameover.component.ts
@@ -13,5 +13,23 @@ export class GameoverComponent {
   goToMenu(){
     this.globalService.player.positionX = 200;
     this.globalService.player.positionY = 200;
+
+    this.globalService.player.currentHealth = 100;
+    this.globalService.currentEnemy.id = 1;
+    this.globalService.greatBeast.currentHealth = 100;
+    this.globalService.hunter1.currentHealth = 200;
+    this.globalService.hunter2.currentHealth = 100;
+    this.globalService.isAxePickedUp = false;
+    this.globalService.isCrowbarPickedUp = false;
+    this.globalService.isFriendlyHelping = false;
+    this.globalService.isGunPickedUp = false;
+    this.globalService.isInCombat = false;
+    this.globalService.isInDialog = false;
+    this.globalService.isKeyPickedUp = false;
+    this.globalService.isPlayerDefending = false;
+    this.globalService.isPlayerTurn = false;
+    this.globalService.isSisterPageRead = false;
+    this.globalService.isTraitorHelping = false;
+    this.globalService.isWantedPageRead = false;
   }
 }
