9e7870717fb167ad47c7f53f42f2f5a9f6471fc3
Blyzaurd <desgagne.guillaume@videotron.ca>
2023-03-12 10:41:10 -0400

    dialog

--- my-app/src/app/dialog/dialog.component.ts
+++ my-app/src/app/dialog/dialog.component.ts
@@ -11,36 +11,55 @@ export class DialogComponent implements OnInit {
 
   currentDialog : string = ''
   currentEnemy : Enemy = this._globalService.currentEnemy;
+  myCurrentDialog : string = '';
+  isSister = this._globalService.isSisterPageRead;
+  isWanted = this._globalService.isWantedPageRead;
 
   constructor(public _globalService : GlobalService) {}
 
   ngOnInit(): void {
     if(this.currentEnemy.id == 1) {
-      this.currentDialog = "Tu ne sera pas un obstacle a mon enrichissement, gamin...";
+      this.currentDialog = this.currentEnemy.name + " : Tu ne sera pas un obstacle a mon enrichissement, gamin...";
     }
     if(this.currentEnemy.id == 2) {
-      this.currentDialog = "Mon carreau saura trouver sa cible.";
+      this.currentDialog = this.currentEnemy.name + " : Mon carreau saura trouver sa cible.";
     }
   }
 
   teamUp() {
+    this.currentDialog = '';
     if(this.currentEnemy.id == 1) {
-      if(this._globalService.isFriendlyHelping) {
-        this.currentDialog = "Séparé la prime en trois part ? Quelle idiotie gamin!"
-      } else {
-        this.currentDialog = "Astucieux gamin... Je suis d'accord.  On divise la prime à part égal.  On se rejoint sur le champ de bataille."
-      }
+      this.myCurrentDialog = "Moi : J'ai vu qu'il avait une prime de 500 livres pour tuer la bete, voudriez-vous faire equipe et partager la prime?"
+      setTimeout(() => {
+        if(this._globalService.isFriendlyHelping) {
+          this.currentDialog = this.currentEnemy.name + " : Séparé la prime en trois part ? Quelle idiotie gamin!"
+        } else {
+          this.currentDialog = this.currentEnemy.name + " : Astucieux gamin... Je suis d'accord.  On divise la prime à part égal.  On se rejoint sur le champ de bataille."
+        }
+      }, 750)
     }
     if(this.currentEnemy.id == 2) {
-      if(this._globalService.isTraitorHelping) {
-        this.currentDialog = "Faire équipe avec un assassin comme lui ? JAMAIS !"
-      } else {
-        this.currentDialog = "Faire équipe ? Tout pour sortir de cet enfer et venger ma soeur."
-      }
+      this.myCurrentDialog = "Moi : Etes-vous la chasseuse qui s'est fait enlever sa soeur par la bete? Voudriez-vous faire equipe?"
+      setTimeout(() => {
+        if(this._globalService.isTraitorHelping) {
+          this.currentDialog = this.currentEnemy.name + " : Faire équipe avec un assassin comme lui ? JAMAIS !"
+        } else {
+          this.currentDialog = this.currentEnemy.name + " : Faire équipe ? Tout pour sortir de cet enfer et venger ma soeur."
+        }
+      }, 750)
     }
   }
 
-  engage() {
+  quit() {
+    this._globalService.isInDialog = false;
+  }
 
+  engage() {
+    if(this.currentEnemy.id === 1) {
+      
+    }
+    if(this.currentEnemy.id === 2) {
+      
+    }
   }
 }
