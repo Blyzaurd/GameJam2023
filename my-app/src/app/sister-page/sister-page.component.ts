8af92fc5a90f1863801e54d18f302ed0389a6839
Blyzaurd <desgagne.guillaume@videotron.ca>
2023-03-12 11:31:47 -0400

    page + debut appcomp

---
+++ my-app/src/app/sister-page/sister-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sister-page',
+  templateUrl: './sister-page.component.html',
+  styleUrls: ['./sister-page.component.css']
+})
+export class SisterPageComponent {
+
+}
---
+++ my-app/src/app/wanted-page/wanted-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wanted-page',
+  templateUrl: './wanted-page.component.html',
+  styleUrls: ['./wanted-page.component.css']
+})
+export class WantedPageComponent {
+
+}
